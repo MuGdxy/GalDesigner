b944cfb724436f8371892a19ed05b1e0a71631c7
Link Clinton <LinkClinton@outlook.com>
2017-08-21 13:21:50 +0800

    Fix Dir

--- Source/APILibrary/Properties/AssemblyInfo.cs
+++ Source/APILibrary/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CoreLibrary")]
+[assembly: AssemblyTitle("APILibrary")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CoreLibrary")]
+[assembly: AssemblyProduct("APILibrary")]
 [assembly: AssemblyCopyright("Copyright ©  2017 Link")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("fffa5e8e-d6b6-4f6d-8334-524a9e12a4c0")]
+[assembly: Guid("9f6b254e-6770-4a79-97f3-671d98618ac0")]
 
 // Version information for an assembly consists of the following four values:
 //
