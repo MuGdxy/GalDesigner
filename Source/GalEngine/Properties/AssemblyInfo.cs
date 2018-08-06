34e7fd54586ba03f6eb7a7cae1cade156d3022c5
LinkClinton <LinkClinton@outlook.com>
2018-08-06 12:31:54 +0800

    Try remake

--- Source/GalEngine/Properties/AssemblyInfo.cs
+++ Source/GalEngine/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("GalEngine")]
-[assembly: AssemblyCopyright("Copyright ©  2018 Link")]
+[assembly: AssemblyCopyright("Copyright © 2018 Link")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- Source/GalEngine/Properties/Resources.Designer.cs
+++ Source/GalEngine/Properties/Resources.Designer.cs
@@ -59,15 +59,5 @@ namespace GalEngine.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Byte[].
-        /// </summary>
-        internal static byte[] SpecialThanks {
-            get {
-                object obj = ResourceManager.GetObject("SpecialThanks", resourceCulture);
-                return ((byte[])(obj));
-            }
-        }
     }
 }
